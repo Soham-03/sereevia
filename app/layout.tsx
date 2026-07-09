67f0a41c7b2c5dc555c1ad51f3bc85c6f9af428e
Soham-03 <sohamparab1003@gmail.com>
2026-07-09 15:36:27 +0530

    -changed product details page title font to poppins

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,6 +17,14 @@ export default function RootLayout({
 }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Poppins:wght@500&display=swap"
+          rel="stylesheet"
+        />
+      </head>
       <body>
         <IntroVideo />
         <HeroBanner
